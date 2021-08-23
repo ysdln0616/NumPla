b1f3d5406db72393dccab356574f57a5c1b9892a
ysdln0616 <1419099@ed.tus.ac.jp>
2021-08-23 19:57:52 +0900

    総当たり1

--- NumPla.js
+++ NumPla.js
@@ -62,6 +62,7 @@ let  queue = [
   
   // 問題パネルのマスが押された時の処理
   function mainClick(e) {
+    // console.log(e);
     if (place != undefined) {
       place.classList.remove("mainClick");
     }
@@ -71,9 +72,10 @@ let  queue = [
   
   // 数字選択のマスが押された時の処理
   function selectClick(e) {
+    // console.log(e);
     place.textContent = e.target.value;
   }
-  
+
   function check1(){
     let checkFlag = true;
     let tr = document.querySelectorAll(".main tr");
@@ -112,29 +114,190 @@ let  queue = [
     return checkFlag;
   }
 
+
+
+
   function answer(){
+    let tr = document.querySelectorAll(".main tr");
+    
     const h2 = document.querySelector("h2");
     if(check1()==false){
       h2.textContent = "おかしい";
       return;
     }else{
-      h2.textContent = "あってる";
+      h2.textContent = "考え中";
     }
-    const tr = document.querySelectorAll(".main tr");
-    console.log(tr[0].querySelectorAll("td")[0].textContent);
-    // 横
-    let s="";
-    let a=0;
-    for(let k=0;k<9;k++){
-        s+=tr[k].textContent;
-        a+=Number(tr[k].textContent);
-        for(let j=0;j<9;j++){
 
+    for(let i=0;i<9;i++){
+      for(let j=0;j<9;j++){
+        let td = tr[i].querySelectorAll("td");
+        if(Number(td[j].textContent)!=0){
+          td[j].classList=[];
+          td[j].classList.add("clickdisable");
+          console.log(td[j].className)
         }
+      }
     }
-    console.log(Number(s));
-    console.log(a);
+
+    // まず、総当たりを考える。
+    all(tr);
+
+    h2.textContent = "おわり";
+
+    // // 横
+    // let s="";
+    // let a=0;
+    // for(let k=0;k<9;k++){
+    //     s+=tr[k].textContent;
+    //     a+=Number(tr[k].textContent);
+    //     for(let j=0;j<9;j++){
+
+    //     }
+    // }
+    // console.log(Number(s));
+    // console.log(a);
   }
+
+  function all(tr){
+    let che =true;
+    let n=3
+    // let tc = tr[8].querySelectorAll("td");
+    // tc[0].click();
+    // tc[0].textContent=0;
+    for(let i=0;i<9;i++){
+      for(let j=0;j<n;j++){
+        let td = tr[i].querySelectorAll("td");
+        if(td[j].className=="clickdisable"){
+          if(che==false){
+            [tri,i,j]=back(tr,i,j);
+          }
+            continue;
+
+          
+          
+        }
+        // if(Number(td[j].textContent)>0&&che==true){
+        //   continue;
+        // }
+        
+        che=true
+        // console.log("(j,i)="+j+i);
+        for(let k=1;k<10;k++){//入れる数字
+          td[j].textContent = k;
+          td[j].click();
+          if(check2(tr)==true){
+            break;
+          }
+          if(k==9){
+            // return;
+
+            // console.log("(j,i)="+j+","+i);
+            // if(j>0){
+            //   tr[i].querySelectorAll("td")[j-1].textContent+=1;
+            //   tr[i].querySelectorAll("td")[j-1].textContent=Number(tr[i].querySelectorAll("td")[j-1].textContent);
+            //   tr[i].querySelectorAll("td")[j-1].click();
+            //   // td[]
+            // }else{
+            //   tr[i-1].querySelectorAll("td")[8].textContent+=1;
+            //   tr[i-1].querySelectorAll("td")[8].textContent=Number(tr[i-1].querySelectorAll("td")[8].textContent);
+            //   tr[i-1].querySelectorAll("td")[8].click();
+            // }
+
+
+
+            [tri,i,j]=back(tr,i,j);
+            td[j].textContent=0;
+            td[j].click();
+            if(j>0){
+              j=j-1;
+            }else{
+              i=i-1;
+              j=n-1;
+              // break;
+            }
+            che=false;
+
+            
+          }
+        }
+        let bi=i;
+        let bj=j;
+      }
+    }
+  }
+
+  function back(tr,i,j){
+  let n=2;
+      if(j>0){
+        if(Number(tr[i].querySelectorAll("td")[j-1].textContent)==9||tr[i].querySelectorAll("td")[j-1].className=="clickdisable"){
+          tr[i].querySelectorAll("td")[j-1].textContent=0;
+          tr[i].querySelectorAll("td")[j-1].click();
+          [tri,i,j]=back(tr,i,j-1);
+        }else{
+          tr[i].querySelectorAll("td")[j-1].textContent=Number(tr[i].querySelectorAll("td")[j-1].textContent)+1;
+          tr[i].querySelectorAll("td")[j-1].click();
+          // console.log("kkk"+tr[5].querySelectorAll("td")[1].textContent)
+        }
+      }else{
+        if(Number(tr[i-1].querySelectorAll("td")[n].textContent)==9||tr[i-1].querySelectorAll("td")[n].className=="clickdisable"){
+          tr[i-1].querySelectorAll("td")[n].textContent=0;
+          tr[i-1].querySelectorAll("td")[n].click();
+          [tri,i,j]=back(tr,i-1,n-1)
+        }else{
+          tr[i-1].querySelectorAll("td")[n].textContent=Number(tr[i-1].querySelectorAll("td")[n].textContent)+1;
+          tr[i-1].querySelectorAll("td")[n].click();
+          // console.log("jjj"+tr[5].querySelectorAll("td")[1].textContent)
+        }
+      }
+      return [tr,i,j];
+
+  }
+
+
+
+  function check2(tr){//入れていいかを決める
+    let checkFlag = true;
+    for(let i=0;i<9;i++){
+      for(let j=0;j<9;j++){
+        for(let k=j+1;k<9;k++){
+          // 横
+          if(Number(tr[i].querySelectorAll("td")[j].textContent)>0&&tr[i].querySelectorAll("td")[j].textContent==tr[i].querySelectorAll("td")[k].textContent){
+            checkFlag=false;
+          }
+          // 縦
+          if(Number(tr[j].querySelectorAll("td")[i].textContent)>0&&tr[j].querySelectorAll("td")[i].textContent==tr[k].querySelectorAll("td")[i].textContent){
+            checkFlag=false;
+            break;
+          }
+        }
+      }
+    }
+
+    for(let i=0;i<9;i++){
+      for(let j=0;j<9;j++){
+        for(let t=i-(i%3);t<i+3-(i%3);t++){
+          for(let k=j-(j%3);k<j+3-(j%3);k++){
+            if(t==i&&k==j){
+              continue;
+            }
+            //3かける3のチェック
+            if(Number(tr[i].querySelectorAll("td")[j].textContent)>0&&tr[i].querySelectorAll("td")[j].textContent==tr[t].querySelectorAll("td")[k].textContent){
+              checkFlag=false;
+              break;
+            }
+          }
+        }
+      }
+    }
+    return checkFlag;
+  }
+
+
+
+
+
+
+
   // 正解判定
   function check() {
     const h2 = document.querySelector("h2");
@@ -176,4 +339,3 @@ let  queue = [
     place.textContent = null;
     console.log(place);
   }
-  
